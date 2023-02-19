842678471e64c39133e0a517604bf4c9264c1402
Irina Ashurova <you-ve_got@mail.ru>
2023-02-19 09:41:02 -0500

    homework

---
+++ myapp/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div style={{backgroundColor: 'pink'}}>
+            Header
+        </div>
+    );
+}
+
+export default Header;
